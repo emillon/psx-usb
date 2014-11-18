7d2f899f69a98c3ed7061b4c769cf2ed54ef1500
Etienne Millon <me@emillon.org>
2014-11-18 22:41:53 +0100

    fill structure

--- psxusb.c
+++ psxusb.c
@@ -14,6 +14,21 @@
 #define PORT PORTC
 #define PIN PINC
 
+#define PSX_LEFT 7
+#define PSX_DOWN 6
+#define PSX_RIGHT 5
+#define PSX_UP 4
+#define PSX_START 3
+#define PSX_SELECT 0
+
+#define PSX_SQUARE 7
+#define PSX_CROSS 6
+#define PSX_CIRCLE 5
+#define PSX_TRIANGLE 4
+#define PSX_R1 3
+#define PSX_L1 2
+#define PSX_R2 1
+#define PSX_L2 0
 
 #define DELAY_CLOCK_US 50
 
@@ -22,10 +37,16 @@ struct joystick_state {
 	int down;
 	int left;
 	int right;
+	int start;
+	int select;
 	int square;
 	int cross;
 	int circle;
 	int triangle;
+	int l1;
+	int l2;
+	int r1;
+	int r2;
 };
 
 static inline void configure_pin_input(int pin)
@@ -100,25 +121,27 @@ static void read_joystick(struct joystick_state *js)
 {
 	signal_down(PSX_PIN_ATT);
 
-	print("Read\n");
-	uint8_t cmd = 0x01;
-	uint8_t byte;
-	byte = transmit(cmd);
-	phex(cmd); print(" -> "); phex(byte); print("\n");
-	cmd = 0x42;
-	byte = transmit(cmd);
-	phex(cmd); print(" -> "); phex(byte); print("\n");
-	cmd = 0x00;
-	byte = transmit(cmd);
-	phex(cmd); print(" -> "); phex(byte); print("\n");
-	cmd = 0x00;
-	byte = transmit(cmd);
-	phex(cmd); print(" -> "); phex(byte); print("\n");
-	cmd = 0x00;
-	byte = transmit(cmd);
-	phex(cmd); print(" -> "); phex(byte); print("\n");
+	transmit(0x01);
+	transmit(0x42);
+	transmit(0x00);
+	uint8_t data1 = transmit(0x00);
+	uint8_t data2 = transmit(0x00);
 
 	signal_up(PSX_PIN_ATT);
+
+	js->up = data1 & (1 << PSX_UP);
+	js->down = data1 & (1 << PSX_DOWN);
+	js->left = data1 & (1 << PSX_LEFT);
+	js->right = data1 & (1 << PSX_RIGHT);
+	js->start = data1 & (1 << PSX_START);
+	js->select = data1 & (1 << PSX_SELECT);
+	js->square = data2 & (1 << PSX_SQUARE);
+	js->cross = data2 & (1 << PSX_CROSS);
+	js->triangle = data2 & (1 << PSX_TRIANGLE);
+	js->l1 = data2 & (1 << PSX_L1);
+	js->l2 = data2 & (1 << PSX_L2);
+	js->r1 = data2 & (1 << PSX_R1);
+	js->r2 = data2 & (1 << PSX_R2);
 }
 
 static void loop(void)
@@ -126,6 +149,31 @@ static void loop(void)
 	_delay_ms(1000);
 	static struct joystick_state js;
 	read_joystick(&js);
+
+#define P(btn) do { \
+	print(#btn); \
+	if(js.btn) { \
+		print("[X]\n"); \
+	} else { \
+		print("[ ]\n"); \
+	} \
+} while(0)
+
+	print("\n\nRead\n");
+	P(up);
+	P(down);
+	P(left);
+	P(right);
+	P(start);
+	P(select);
+	P(square);
+	P(cross);
+	P(circle);
+	P(triangle);
+	P(l1);
+	P(l2);
+	P(r1);
+	P(r2);
 }
 
 int main(void)
